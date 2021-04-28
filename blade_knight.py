26d588af4f09501c929a52159f25f63398e5c5a5
valeri1383 <valeri1383@gmail.com>
2021-04-28 17:14:31 +0100

    Delete blade_knight.py

--- blade_knight.py
+++
@@ -1,6 +0,0 @@
-#from Python_OOP.Inheritance_Exercise.Players_and_Monsters.project.dark_knight import DarkKnight
-from project.dark_knight import DarkKnight
-
-
-class BladeKnight(DarkKnight):
-    pass
