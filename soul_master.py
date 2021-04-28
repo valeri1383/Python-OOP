735ea3ea5066185e34aefbc396e62eebc1d44627
valeri1383 <valeri1383@gmail.com>
2021-04-28 17:15:21 +0100

    Delete soul_master.py

--- soul_master.py
+++
@@ -1,6 +0,0 @@
-#from Python_OOP.Inheritance_Exercise.Players_and_Monsters.project.dark_wizard import DarkWizard
-from project.dark_wizard import DarkWizard
-
-
-class SoulMaster(DarkWizard):
-    pass
