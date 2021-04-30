1a12c112098071513613b367cd11f3960defb929
valeri1383 <valeri1383@gmail.com>
2021-04-30 16:26:44 +0100

    Delete Reverse string.py

--- Inheritance_Lab/Reverse string.py
+++
@@ -1,9 +0,0 @@
-def reverse_text(text):
-    counter = len(text) - 1
-    while counter >= 0:
-        yield text[counter]
-        counter -= 1
-
-
-for char in reverse_text("step"):
-    print(char, end='')
